5f085fccb1cf4d237a74a66eae37ec6191af0780
Theo Kostelijk <tkostelijk@users.noreply.github.com>
2022-02-11 18:00:31 +0100

    Update GithubDemo.ps1
    New if statement.

--- AA/GithubDemo.ps1
+++ AA/GithubDemo.ps1
@@ -2,14 +2,17 @@
 Param(
   $message
 )
+Write-Output "Jobid: $($PSPrivateMetadata.JobId)"
 
 Write-Output $message
 
 
-if($PSPrivateMetadata){
-  Write-Output "Jobid: $($PSPrivateMetadata.JobId)"
+
+if($PWD -like "c:\temp\){
+  Write-Output "This script runs on an Azure Worker"
 }
 else{
+  Write-Output "This script runs on a Hybrid Worker"
   Write-Output "I am"
   WhoAmi /all
 }
